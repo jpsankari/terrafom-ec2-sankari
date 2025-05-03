4714d8f821a855eb23816366cdbe21d1b741394e
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-05-03 18:42:19 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,58 @@
-resource "aws_vpc" "ce10-vpn-sankari" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "ce10-vpc-sankari" {
+  cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name = "ce10-vpn-sankari"
+    Name = "ce10-vpc-sankari"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "ce10-sankari-IGW" {
+  vpc_id = aws_vpc.ce10-vpc-sankari.id
+
+  tags = {
+    Name = "ce10-sankari-IGW"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "ce10-sankari-public" {
+  vpc_id = aws_vpc.ce10-vpc-sankari.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ce10-sankari-IGW.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
   }
 }
 
 # Subnet
-resource "aws_subnet" "subnet_sankari_public" {
-  vpc_id                  = aws_vpc.ce10-vpn-sankari.id
-  cidr_block              = "10.0.1.0/24"
+resource "aws_subnet" "ce10_sankari_publicSubnet1" {
+  vpc_id                  = aws_vpc.ce10-vpc-sankari.id
+  cidr_block              = "10.0.0.0/28"
   availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet_sankari_public"
+    Name = "ce10_sankari_publicSubnet1"
   }
+}
+
+
+# Associate Public Subnet with Route Table
+resource "aws_route_table_association" "ce10-publicRT" {
+  subnet_id      = aws_subnet.ce10_sankari_publicSubnet1.id
+  route_table_id = aws_route_table.ce10_sankari_publicSubnet1.id
 }
 
 # Security Group (Allow SSH)
 resource "aws_security_group" "ce10_sg_sankari_allow_ssh" {
-  name        = "sankari_allow_ssh"
+  name        = "ce10_sg_sankari_allow_ssh"
   description = "Allow SSH"
-  vpc_id      = aws_vpc.ce10-vpn-sankari.id
+  vpc_id      = aws_vpc.ce10-vpc-sankari.id
 
   ingress {
     from_port   = 22
