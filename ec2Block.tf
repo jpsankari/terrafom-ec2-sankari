1ee584aa7972ee6c78c8c1726549f2552aa74f4d
jpsankari <jpsankari@hotmail.com>
2025-05-03 19:28:16 +0800

    Your commit message

--- ec2Block.tf
+++ ec2Block.tf
@@ -4,4 +4,7 @@ resource "aws_instance" "sankari_bastionServer" {
   subnet_id                   = aws_subnet.ce10_sankari_publicSubnet1.id
   vpc_security_group_ids = [aws_security_group.ce10_sg_sankari_allow_ssh.id]
   key_name                    = "sankariKPMay2" # Replace with your actual EC2 key pair name
+ tags = {
+    Name = "sankari_bastionServer"
+  }
 }
